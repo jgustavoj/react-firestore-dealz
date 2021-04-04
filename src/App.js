f1d070c0608bb5ca0acb81f51f616db3bd3c552e
jgustavoj <gjime011@fiu.edu>
2021-04-04 15:01:53 -0400

    initial commit with db working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import firebase from "./firebase";
+import "./App.css";
 
 function App() {
+  const [deals, setDeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const ref = firebase.firestore().collection("deals");
+  // console.log(ref);
+
+  // this function uses querySnapshot and it gives a response in "realtime" from firestore.
+  function getDeals() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setDeals(items);
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    getDeals();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="content">
+        <h1>Dealz</h1>
+        {deals.map((deal) => (
+          <div key={deal.id}>
+            {/* <h1 className="company"> {deal.Company}</h1> */}
+            {deal.Company !== "" ? (
+              <h2 className="image__title">
+                {deal.Company} : {deal.Title}
+              </h2>
+            ) : (
+              ""
+            )}
+            <div className="image">
+              <a href={deal.Link} target="blank">
+                <img className="rounded img-fluid" src={deal.Link} alt=""></img>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
